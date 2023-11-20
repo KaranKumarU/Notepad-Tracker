03fd02cff31dec2c39ec61f4a03f09ac114281ec
KaranKumarU <karankumar.u@datagrokr.com>
2023-11-20 14:12:06 +0530

    updated notepad tracker app and template

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-import subprocess
-from flask import Flask, render_template, request, redirect, url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+)
 import os
+import subprocess
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["ALLOWED_EXTENSIONS"] = {"txt", "md"}  # Specify allowed file extensions
 
 
 @app.route("/")
@@ -15,15 +25,24 @@ def save_note():
     note_text = request.form.get("note_text")
 
     # Save the note to a file
-    with open("note.txt", "w") as file:
+    filename = secure_filename("note.txt")
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
+    with open(file_path, "w") as file:
         file.write(note_text)
 
     # Commit changes to Git
-    subprocess.run(["git", "add", "note.txt"])
-    subprocess.run(["git", "commit", "-m", "Auto commit: Save note"])
+    subprocess.run(["git", "add", file_path])
+    subprocess.run(["git", "commit", "-m", f"Auto commit: Save note ({filename})"])
 
     return redirect(url_for("index"))
 
 
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+
+
 if __name__ == "__main__":
+    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.run(debug=True)
