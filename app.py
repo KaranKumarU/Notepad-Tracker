4fa65a3539e7829559869fc1ced9d18d71c4fe7e
KaranKumarU <karankumar.u@datagrokr.com>
2023-11-20 17:04:23 +0530

    updated notepad tracker app

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import (
-    Flask,
-    render_template,
-    request,
-    redirect,
-    url_for,
-    send_from_directory,
-)
+from flask import Flask, render_template, request, send_from_directory
 import os
 import subprocess
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "uploads"
-app.config["ALLOWED_EXTENSIONS"] = {"txt", "md"}  # Specify allowed file extensions
+
+UPLOADS_FOLDER = "uploads"
+app.config["UPLOADS_FOLDER"] = UPLOADS_FOLDER
 
 
 @app.route("/")
@@ -25,24 +18,29 @@ def save_note():
     note_text = request.form.get("note_text")
 
     # Save the note to a file
-    filename = secure_filename("note.txt")
-    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-
-    with open(file_path, "w") as file:
+    with open("note.txt", "w") as file:
         file.write(note_text)
 
-    # Commit changes to Git
-    subprocess.run(["git", "add", file_path])
-    subprocess.run(["git", "commit", "-m", f"Auto commit: Save note ({filename})"])
+    # Commit the changes to the Git repository
+    subprocess.run(["git", "add", "note.txt"])
+    subprocess.run(["git", "commit", "-m", "Update note"])
 
-    return redirect(url_for("index"))
+    # Handle file upload
+    if "file" in request.files:
+        uploaded_file = request.files["file"]
+        if uploaded_file.filename != "":
+            file_path = os.path.join(
+                app.config["UPLOADS_FOLDER"], uploaded_file.filename
+            )
+            uploaded_file.save(file_path)
+
+    return render_template("index.html", message="Note and file saved successfully.")
 
 
 @app.route("/uploads/<filename>")
 def uploaded_file(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+    return send_from_directory(app.config["UPLOADS_FOLDER"], filename)
 
 
 if __name__ == "__main__":
-    os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
     app.run(debug=True)
