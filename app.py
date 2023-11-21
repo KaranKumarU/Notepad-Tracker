c905db4525e0c3b4246126cd59b4259ee0906ccc
KaranKumarU <karankumar.u@datagrokr.com>
2023-11-21 22:49:21 +0530

    updated add and templates

--- app.py
+++ app.py
@@ -1,45 +1,64 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for
 import os
-import subprocess
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "notes"
 
-UPLOADS_FOLDER = "uploads"
-app.config["UPLOADS_FOLDER"] = UPLOADS_FOLDER
+if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+    os.makedirs(app.config["UPLOAD_FOLDER"])
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    notes = get_note_list()
+    return render_template("index.html", notes=notes)
 
 
-@app.route("/save_note", methods=["POST"])
-def save_note():
-    note_text = request.form.get("note_text")
+@app.route("/create", methods=["POST"])
+def create_note():
+    note_name = request.form["note_name"]
+    note_path = os.path.join(app.config["UPLOAD_FOLDER"], f"{note_name}.txt")
 
-    # Save the note to a file
-    with open("note.txt", "w") as file:
-        file.write(note_text)
+    with open(note_path, "w") as file:
+        file.write("")
 
-    # Commit the changes to the Git repository
-    subprocess.run(["git", "add", "note.txt"])
-    subprocess.run(["git", "commit", "-m", "Update note"])
-
-    # Handle file upload
-    if "file" in request.files:
-        uploaded_file = request.files["file"]
-        if uploaded_file.filename != "":
-            file_path = os.path.join(
-                app.config["UPLOADS_FOLDER"], uploaded_file.filename
-            )
-            uploaded_file.save(file_path)
-
-    return render_template("index.html", message="Note and file saved successfully.")
+    return redirect(url_for("index"))
 
 
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    return send_from_directory(app.config["UPLOADS_FOLDER"], filename)
+@app.route("/upload", methods=["POST"])
+def upload_note():
+    uploaded_file = request.files["file"]
+    if uploaded_file.filename != "":
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], uploaded_file.filename)
+        uploaded_file.save(file_path)
+
+    return redirect(url_for("index"))
+
+
+@app.route("/edit/<note_name>", methods=["GET", "POST"])
+def edit_note(note_name):
+    note_path = os.path.join(app.config["UPLOAD_FOLDER"], f"{note_name}.txt")
+
+    if request.method == "GET":
+        with open(note_path, "r") as file:
+            content = file.read()
+
+        return render_template("edit.html", note_name=note_name, content=content)
+
+    elif request.method == "POST":
+        new_content = request.form["new_content"]
+        with open(note_path, "w") as file:
+            file.write(new_content)
+
+        return redirect(url_for("index"))
+
+
+def get_note_list():
+    notes = []
+    for filename in os.listdir(app.config["UPLOAD_FOLDER"]):
+        if filename.endswith(".txt"):
+            notes.append(os.path.splitext(filename)[0])
+    return notes
 
 
 if __name__ == "__main__":
